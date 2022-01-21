30676501ed0f3dbdb75a8dfdfe61521b52d6dfe0
hanju <schw240@naver.com>
2022-01-21 15:25:40 +0900

    [S06P12A407-20] [BE] feat: spring boot initial setting
    - backend framework spring boot initial setting upload

---
+++ pollar/pollar/src/main/java/com/ssafy/pollar/PollarApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.pollar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollarApplication.class, args);
+	}
+
+}
