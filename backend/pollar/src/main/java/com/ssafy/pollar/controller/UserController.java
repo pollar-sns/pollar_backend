baae2248ca62509e564c2661e3f12588158ee054
정홍진 <mistake9518@gmail.com>
2022-01-25 10:51:42 +0900

    [S06P12A407-34] [BE] feat: idcheck, nickcheck, interestcheck
    - [S06PA12407-36] [BE] feat: idcheck duplicated check
    - [S06PA12407-39] [BE] feat: interest category select
    - [S06PA12407-46] [BE] feat: nickname duplicated check

--- backend/pollar/src/main/java/com/ssafy/pollar/controller/UserController.java
+++ backend/pollar/src/main/java/com/ssafy/pollar/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ssafy.pollar.controller;
 
 import com.ssafy.pollar.model.dto.UserDto;
 import com.ssafy.pollar.model.service.UserService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/user")
@@ -26,9 +28,39 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<String> signup (@RequestBody @ApiParam(value = "회원가입시 필요한 회원정보", required = true) UserDto userDto )throws Exception {
         userService.signup(userDto);
-
-
+//        if(userService.nicknameCheck((userDto.getUserNickname()))){
+//        }else{
+//            return new ResponseEntity<String>(FAIL,HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
         return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "아이디중복체크")
+    @GetMapping("/idcheck")
+    public ResponseEntity<Boolean> idcheck(@RequestParam String userId) throws Exception{
+        boolean checkflag = false;
+        if(userService.idCheck((userId))){
+            checkflag= true;
+        }
+        return new ResponseEntity<Boolean>(checkflag,HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "닉네임중복체크")
+    @GetMapping("/nickcheck")
+    public ResponseEntity<Boolean> nicknamecheck(@RequestParam String userNickname) throws Exception{
+        boolean checkflag = false;
+        if(userService.nicknameCheck((userNickname))){
+            checkflag= true;
+        }
+        return new ResponseEntity<Boolean>(checkflag,HttpStatus.OK);
+    }
+
+    @PostMapping("/emailcheck")
+    public ResponseEntity<Boolean> emailcheck(@RequestParam String userEmail) throws Exception {
+        boolean checkflag = false;
+        if(userService.emailCheck(userEmail)){
+
+        }
+        return new ResponseEntity<Boolean>(checkflag,HttpStatus.OK);
+    }
 }
--- backend/pollar/src/main/java/com/ssafy/pollar/model/repository/UserRepository.java
+++ backend/pollar/src/main/java/com/ssafy/pollar/model/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.ssafy.pollar.model.repository;
 import com.ssafy.pollar.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,String> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User,String> {
+    Optional<User> findByUserId(String userId);
+    Optional<User> findByUserNickname(String userNickname);
+    Optional<User> findByUserEmail(String userEmail);
 
 }
