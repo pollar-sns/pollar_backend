7cfe1f630078f4b2222ccc2ea6171fa4870beb25
정홍진 <mistake9518@gmail.com>
2022-01-25 10:51:42 +0900

    [S06P12A407-34] [BE] feat: idcheck, nickcheck, interestcheck
    - [S06PA12407-36] [BE] feat: idcheck duplicated check
    - [S06PA12407-39] [BE] feat: interest category select
    - [S06PA12407-46] [BE] feat: nickname duplicated check

--- backend/pollar/src/main/java/com/ssafy/pollar/model/service/UserService.java
+++ backend/pollar/src/main/java/com/ssafy/pollar/model/service/UserService.java
@@ -5,5 +5,7 @@ import com.ssafy.pollar.model.dto.UserDto;
 public interface UserService {
 
     void signup(UserDto userDto) throws Exception;
-
+    boolean idCheck(String userId) throws Exception;
+    boolean nicknameCheck(String userNickname) throws Exception;
+    boolean emailCheck(String userEmail) throws Exception;
 }
