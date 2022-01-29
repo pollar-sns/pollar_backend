82f734e763b7b5a5ed222993e1cda3704c0354a1
DongKyu Kim <aass0903@naver.com>
2022-01-29 19:46:22 +0900

    [S06P12A407-118] [BE] feat: connect swagger
    - connection swagger using springdoc
    - example of api docs in Vote Controller, VoteDto and UserController of
      idCheck

--- backend/pollar/src/main/java/com/ssafy/pollar/model/dto/VoteDto.java
+++ backend/pollar/src/main/java/com/ssafy/pollar/model/dto/VoteDto.java
@@ -6,6 +6,7 @@ import com.ssafy.pollar.domain.entity.VoteLike;
 import com.ssafy.pollar.domain.entity.VoteSelect;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,30 +18,30 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel(value = "VoteDto : 피드정보", description = "피드의 상세 정보를 나타낸다.")
+//@ApiModel(value = "VoteDto : 피드정보", description = "피드의 상세 정보를 나타낸다.")
 public class VoteDto {
-    @ApiModelProperty(value = "피드 아이디. 자동으로 생성되므로 작성 보내지 않는다")
+    @Schema(description = "피드 아이디. 자동으로 생성되므로 작성 보내지 않는다")
     private Long voteId;
-    @ApiModelProperty(value = "피드 작성자 이름.")
+    @Schema(description = "피드 작성자 이름.", example = "홍길동")
     private String voteName;
-    @ApiModelProperty(value = "피드 내용.")
+    @Schema(description = "피드 내용.", example = "여자친구가 민초단인데 계속 만나야 될까요?")
     private String voteContent;
-    @ApiModelProperty(value = "투표 타입. 익명성 여부 판단.")
+    @Schema(description = "투표 타입. 익명성 여부 판단.", example = "false")
     private Boolean voteType;
-    @ApiModelProperty(value = "투표 마감 시간.")
+    @Schema(description = "투표 마감 시간.")
     private Date voteExpirationTime;
-    @ApiModelProperty(value = "작성자 익명 여부.")
+    @Schema(description = "작성자 익명 여부.")
     private Boolean userAnonymouseType;
-    @ApiModelProperty(value = "투표 익명 여부.")
+    @Schema(description = "투표 익명 여부.")
     private Boolean voteAnonymouseType;
-    @ApiModelProperty(value = "투표 생성 시간.")
+    @Schema(description = "투표 생성 시간.")
     private LocalDateTime voteCreateTime;
-    @ApiModelProperty(value = "투표에 해당하는 카테고리 리스트.")
+    @Schema(description = "투표에 해당하는 카테고리 리스트.")
     private List<VoteCategory> voteCategories;
-    @ApiModelProperty(value = "좋아요 받은 투표.")
+    @Schema(description = "좋아요 받은 투표.")
     private List<VoteLike> iked;
-    @ApiModelProperty(value = "피드에 달린 댓글들.")
+    @Schema(description = "피드에 달린 댓글들.")
     private List<Reply> voteReplys;
-    @ApiModelProperty(value = "참여한 투표.")
+    @Schema(description = "참여한 투표.")
     private List<VoteSelect> VoteSelects;
 }
