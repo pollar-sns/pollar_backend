164612428e589a4e9e8b10bb7a721a1ce783302e
ziu974 <ziu974@gmail.com>
2022-02-03 17:16:24 +0900

    [S06P12A407-40] [FE] feat: interest selection in signup page
    - add interest selection in signup

--- frontend/src/components/signup/SelectInterests.js
+++ frontend/src/components/signup/SelectInterests.js
@@ -1,40 +1,78 @@
 import { useEffect, useState } from 'react';
 import { getAllCategories } from '../../services/api/CategoryApi';
-import Chip from '@mui/material/Chip';
+import { Chip, List, Stack, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 
 const ListItem = styled('li')(({ theme }) => ({
   margin: theme.spacing(0.5),
 }));
 
-export default function SelectInterests() {
+export default function SelectInterests({ signupInfo }) {
+  // 전체 카테고리
   const [categoryList, setCategoryList] = useState([]);
+  // 사용자 선택 관심분야
+  const [interestList, setInterestList] = useState([]);
 
-  // const getList = async () => {
-  //   const list = await getAllCategories();
-  //   console.log(list);
-  //   setCategoryList(list);
-  // };
-  // useEffect(() => {
-  //   getList();
-  // }, []);
-
-  // todo
-  const selectCategory = () => {
-    console.log('선택');
+  const getList = async () => {
+    const list = await getAllCategories();
+    setCategoryList(list);
   };
+
+  /* 관심분야 선택 해제 */
+  const handleDelete = (categoryNameSmall) => {
+    const filteredList = interestList.filter(
+      (item) => item.categoryNameSmall !== categoryNameSmall
+    );
+    setInterestList(filteredList);
+  };
+
+  /* 관심분야 선택 추가 */
+  const selectCategory = (item) => {
+    if (interestList.length >= 3) {
+      // todo
+      alert('최대 3개 선택가능');
+    } else {
+      const selected = item;
+      setInterestList((currentArray) => [selected, ...currentArray]);
+    }
+  };
+
+  /* 관심분야 선택완료 시 */
+  // todo
+
+  useEffect(() => {
+    // 관심분야 목록 API 호출
+    getList();
+  }, []);
+
   return (
     <>
-      {/* <ListItem key={data.key}>
-        <Chip
-          icon={icon}
-          label={data.label}
-          onDelete={data.label === 'React' ? undefined : handleDelete(data)}
-        />
-      </ListItem> */}
-      {categoryList.map((category) => {
-        <Chip label={category} onClick={selectCategory} />;
-      })}
+      <Typography variant="body2" align="left" sx={{ color: 'text.secondary', mt: 3 }}>
+        선택한 관심분야
+      </Typography>
+      <Stack direction="row" spacing={1}>
+        {interestList.map((item, index) => (
+          <Chip
+            key={index * 100}
+            label={item.categoryNameSmall}
+            onDelete={() => handleDelete(item.categoryNameSmall)}
+            color="primary"
+            // onDelete={false ? undefined : handleDelete(index)}
+          />
+        ))}
+      </Stack>
+      <Typography variant="body2" align="left" sx={{ color: 'text.secondary', mt: 3 }}>
+        카테고리 전체목록
+      </Typography>
+      <Stack direction="row" spacing={1}>
+        {categoryList.map((item) => (
+          <Chip
+            key={item.categoryId}
+            label={item.categoryNameSmall}
+            onClick={() => selectCategory(item)}
+          />
+        ))}
+      </Stack>
     </>
   );
 }
