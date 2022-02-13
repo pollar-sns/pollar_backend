29c5cd50acf64761bb4839eaa44f07b98fb0259e
ziu974 <ziu974@gmail.com>
2022-02-13 23:24:29 +0900

    [S06P12A407-93] [FE] feat: user voted list
    - voted list vote component
    - modify redirect to profile page in follow list

--- frontend/src/components/profile/VotesTabPanel.js
+++ frontend/src/components/profile/VotesTabPanel.js
@@ -1,16 +1,33 @@
 import { Container, Grid } from '@mui/material';
 import PollVoteCard from '../polls/PollVoteCard';
-import posts from '../../_mocks_/blog';
+import { useEffect, useState } from 'react';
+import { getUserVotesList } from '../../services/api/ProfileApi';
+import { getLoggedUserId } from '../../utils/loggedUser';
 // ----------------------------------------------------------------------
 
-export default function VotesTabPanel() {
+export default function VotesTabPanel({ userId }) {
+  const [pollList, setPollList] = useState([]);
+
+  /* 사용자가 투표한 투표 목록 요청 */
+  const getVotesList = async () => {
+    const list = await getUserVotesList(userId);
+    setPollList(list);
+  };
+
+  useEffect(() => {
+    getVotesList();
+  }, []);
+
   return (
     <>
       <Container>
         <Grid container spacing={3}>
-          {posts.map((post, index) => (
-            <PollVoteCard key={post.id} post={post} index={index} />
-          ))}
+          {/* 투표자 익명 투표일 경우에는 숨김처리 (사용자 본인 프로필일 경우 X) */}
+          {pollList.map((poll, index) =>
+            !poll.voteAnonymousType || userId === getLoggedUserId() ? (
+              <PollVoteCard key={poll.voteId} poll={poll} index={index} />
+            ) : null
+          )}
         </Grid>
       </Container>
     </>
